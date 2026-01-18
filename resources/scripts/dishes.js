13b3c097b25ac16e6ad24228140e29c6a95e2b10
LIGunna93 <lilunga93@googlemail.com>
2026-01-18 07:05:40 +0100

    Adapted dishes.js script and pulled workflow runner action from origin

--- resources/scripts/dishes.js
+++ resources/scripts/dishes.js
@@ -1,5 +1,5 @@
 
 const siteBack = document.getElementsByTagName('html');
-//siteBack.stlye.backgroundImage = url("https://images.unsplash.com/photo-1735190093631-d66ecd1bc433?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Zm9vZCUyMGVuc2VtYmxlc3xlbnwwfHwwfHx8MA%3D%3D");
+siteBack.stlye.backgroundImage = url("https://fastly.picsum.photos/id/1060/536/354.jpg?blur=2&hmac=0zJLs1ar00sBbW5Ahd_4zA6pgZqCVavwuHToO6VtcYY");
 
 const dishList = document.getElementsByClassName('dishList');
