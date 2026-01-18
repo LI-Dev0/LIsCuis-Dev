e6aaddb551795d091d25a36d252a7b487cef9f7f
LIGunna93 <lilunga93@googlemail.com>
2026-01-18 07:10:04 +0100

    Adapted dishes.js script

--- resources/scripts/dishes.js
+++ resources/scripts/dishes.js
@@ -1,5 +1,6 @@
 
 const siteBack = document.getElementsByTagName('html');
-siteBack.stlye.backgroundImage = url("https://fastly.picsum.photos/id/1060/536/354.jpg?blur=2&hmac=0zJLs1ar00sBbW5Ahd_4zA6pgZqCVavwuHToO6VtcYY");
+
+siteBack.stlye.backgroundImg = url("https://fastly.picsum.photos/id/1060/536/354.jpg?blur=2&hmac=0zJLs1ar00sBbW5Ahd_4zA6pgZqCVavwuHToO6VtcYY");
 
 const dishList = document.getElementsByClassName('dishList');
